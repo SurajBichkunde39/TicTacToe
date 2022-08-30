1258868d6d69c37975edbb4c223233d721ad193b
Suraj Bichkunde <surajbichkunde@Surajs-MacBook-Air.local>
2022-08-30 10:19:22 -0700

    Add fragment for home screen
    * Created an empty fragment `HomeScreen`.
    * Added a layout for the fragment.
    * Added the same fragment in MainActivity.

---
+++ app/src/main/java/com/example/tictactoe/HomeScreen.kt
@@ -0,0 +1,37 @@
+package com.example.tictactoe
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A [Fragment] subclass for home screen.
+ * Use the [HomeScreen.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeScreen : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment HomeScreen.
+         */
+        @JvmStatic
+        fun newInstance() = HomeScreen()
+    }
+}
--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.tictactoe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if(savedInstanceState == null){
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.home_screen_container, HomeScreen.newInstance())
+                .commitNow()
+        }
     }
 }
