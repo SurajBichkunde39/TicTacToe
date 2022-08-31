f1a1cb1cf1df71b083ba8d8b5a640dfa9bb7ff18
Suraj Bichkunde <surajbichkunde@Surajs-MacBook-Air.local>
2022-08-31 03:52:35 -0700

    Updated player score to `var` instaid of `val`.

--- app/src/main/java/com/example/tictactoe/models/Player.kt
+++ app/src/main/java/com/example/tictactoe/models/Player.kt
@@ -2,7 +2,7 @@ package com.example.tictactoe.models
 
 data class Player(
     var placeHolderMark: PlaceholderMark,
-    val score: Int = 0,
+    var score: Int = 0,
 )
 
 enum class PlaceholderMark {
