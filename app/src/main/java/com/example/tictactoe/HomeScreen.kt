166699aa4983133d20234fe07b949d2a24e45ee1
Suraj Bichkunde <surajbichkunde@Surajs-MacBook-Air.local>
2022-08-30 23:54:24 -0700

    Add UI to get the data to start the match
    * Added spinner view to select the number of matches in the current session of the game.
    * Added UI where player can select the placeholder.
    * Added start game button.

--- app/src/main/java/com/example/tictactoe/HomeScreen.kt
+++ app/src/main/java/com/example/tictactoe/HomeScreen.kt
@@ -2,20 +2,51 @@ package com.example.tictactoe
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 /**
  * A [Fragment] subclass for home screen.
  * Use the [HomeScreen.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeScreen : Fragment(R.layout.fragment_home_screen) {
+class HomeScreen :
+    Fragment(R.layout.fragment_home_screen),
+    AdapterView.OnItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val spinner: Spinner = view.findViewById(R.id.number_of_sessions_spinner)
+        setUpSpinner(spinner)
+    }
+
+    private fun setUpSpinner(spinner: Spinner) {
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.number_of_matches_in_session,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+    }
+
+    // start: AdapterView.OnItemSelectedListener
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+    // end
 
     companion object {
         /**
